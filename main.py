b9c8078e7d1a35ea0b5ae2d10ba6e8ca8f2ba5e5
Thazulk <thazulk@gmail.com>
2025-05-27 14:30:05 +0200

    Add word count functionality and improve main script usability
    - Implemented get_num_words function in stats.py to count words in text.
    - Updated main.py to accept book path as a command-line argument and provide usage instructions.
    - Enhanced report formatting for better readability.

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-def main(): 
-    book_path = 'books/frankenstein.txt'
+from stats import get_num_words
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
-    num_words = count_words(text)
+    num_words = get_num_words(text)
     num_letters = count_letters(text)
+    # print(f"Found {num_words} total words")
     report_text(num_letters, num_words, book_path)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-        
-def count_words(text):
-        words = text.split()
-        return len(words)
+
 
 def count_letters(text):
     counted_letters = {}
@@ -23,23 +29,29 @@ def count_letters(text):
             counted_letters[letter] = 1
     return counted_letters
 
+
 def sort_on(dict):
-     return dict["num"]
+    return dict["num"]
+
 
 def report_text(letters, words, path):
     letters_dict = []
-    
+
     for letter, num in letters.items():
         if letter.isalpha():
             letters_dict.append({"name": letter, "num": num})
-    letters_dict.sort(reverse=True, key=sort_on)   
-    print(f" --- Begin report of {path} ---")
-    print(f"{words} words found in the book")
-    print("")
+    letters_dict.sort(reverse=True, key=sort_on)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
 
     for item in letters_dict:
-        print(f"The '{item['name']}' character was found {item['num']} times")
+        print(f"{item['name']}: {item['num']}")
 
-    print(" --- End of report ---") 
+    print("============= END ===============")
+
 
 main()
+
